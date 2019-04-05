b57d44fb433df5a181af3f30fdf5d817e3b562fb
Rita <rita@trafi.com>
2019-04-05 21:31:53 +0300

    Add files via upload

---
+++ 7-pamoka/Point3D.java
@@ -0,0 +1,31 @@
+package pamoka.pirma;
+
+public class Point3D extends Point2D {
+
+    private float z;
+
+    Point3D() {
+    }
+
+    Point3D(float x, float y, float z) {
+        super(x, y);
+        this.z = z;
+    }
+
+    public float getZ() {
+        return z;
+    }
+
+    public void setZ(float z) {
+        this.z = z;
+    }
+
+    public void setXYZ(float x, float y, float z) {
+        super.setXY(x, y);
+        this.z = z;
+    }
+
+    public float[] getXYZ() {
+        return new float[]{this.getX(), this.getY(), this.z};
+    }
+}
