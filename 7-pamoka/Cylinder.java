4126b58f5320ae3c3fad4145cf5ec60c6206b125
Rita <rita@trafi.com>
2019-04-05 21:26:02 +0300

    Add files via upload

---
+++ 7-pamoka/Cylinder.java
@@ -0,0 +1,34 @@
+package pamoka.pirma;
+
+public class Cylinder extends Circle {
+
+    private double height;
+
+    Cylinder() { }
+
+    Cylinder(double radius) {
+        super(radius);
+    }
+
+    Cylinder(double radius, double height) {
+        super(radius);
+        this.height = height;
+    }
+
+    Cylinder(double radius, double height, String color) {
+        super(radius, color);
+        this.height = height;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public double getVolume() {
+        return super.getArea() * height;
+    }
+}
